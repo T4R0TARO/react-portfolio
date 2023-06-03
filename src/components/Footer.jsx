ae0a91d0c6b9529566ce8d54651dd348a699b5a5
josh manansala <joshmanansalagsd@gmail.com>
2023-06-02 18:25:01 -0700

    update footer styles and mobile styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import "../styles/Footer.css";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>Footer</p>
+      <div className="footer__container">
+        <p className="footer__copyrite">
+          &copy; 2023{" "}
+          <a href="#" className="copyright__link">
+            joshManansala
+          </a>
+          . All Rights Reserved
+        </p>
+        <ul className="footer__list">
+          <li>
+            <a
+              href="https://github.com/T4R0TARO"
+              target="_blank"
+              className="footer__link"
+            >
+              Github
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/joshua-manansala-59a40279/"
+              target="_blank"
+              className="footer__link"
+            >
+              Linkindn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/taro_code"
+              target="_blank"
+              className="footer__link"
+            >
+              Twitter
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
